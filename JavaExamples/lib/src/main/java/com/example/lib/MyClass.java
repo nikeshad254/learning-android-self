792a6c0f1041993bf5fd4410126a022b4473ff7f
Nikesh Adhikari <nikeshad254@gmail.com>
2024-02-09 18:45:53 +0545

    learning android started

---
+++ JavaExamples/lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,14 @@
+package com.example.lib;
+
+public class MyClass {
+    // my first class and comment
+
+    // this is my main function
+    public static void main(String args[]) {
+        greetHello();
+    }
+
+    static void greetHello(){
+        System.out.println("Hello world");
+    }
+}
