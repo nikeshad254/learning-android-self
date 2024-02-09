8c24ff2e05b74ef794a58aec227cd4082f02ae37
Nikesh Adhikari <nikeshad254@gmail.com>
2024-02-09 18:55:37 +0545

    class car made and object instance created in main method

--- JavaExamples/lib/src/main/java/com/example/lib/MyClass.java
+++ JavaExamples/lib/src/main/java/com/example/lib/MyClass.java
@@ -6,9 +6,31 @@ public class MyClass {
     // this is my main function
     public static void main(String args[]) {
         greetHello();
+
+        Car c = new Car();
+        c.accelerate();
+        c.accelerate();
+        c.brake();
     }
 
     static void greetHello(){
-        System.out.println("Hello world");
+        System.out.println("Hello to speed world");
+    }
+}
+
+class Car{
+    int speed;
+
+    void tellSpeed(){
+        System.out.println("Your speed is now: "+ speed);
+    }
+    void accelerate(){
+        speed +=10;
+        tellSpeed();
+    }
+
+    void brake(){
+        speed = 0;
+        tellSpeed();
     }
 }
